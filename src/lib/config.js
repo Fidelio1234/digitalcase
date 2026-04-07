1b59fbcfe5910e679adec5798da3d14a1cda836b
Fidelio1234 <demivanlecce@gmail.com>
2026-04-07 18:33:59 +0200

    fix: forza redeploy Vercel

--- src/lib/config.js
+++ src/lib/config.js
@@ -1,3 +1,4 @@
 // ID negozio corrente
 // NEXT_PUBLIC_ prefix richiesto per essere visibile nel browser
 export const NEGOZIO_ID = process.env.NEXT_PUBLIC_NEGOZIO_ID || 'd7861145-dfb7-4b28-ae10-b35166231128'
+// Mar  7 Apr 2026 18:33:59 CEST
