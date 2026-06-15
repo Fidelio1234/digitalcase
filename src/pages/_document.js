469defa897aa5f6a1f10a3a74295c53583da1b82
Fidelio1234 <demivanlecce@gmail.com>
2026-06-15 20:51:36 +0200

    feat: PWA manifest e icone app

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,6 +5,12 @@ export default function Document() {
     <Html lang="it" translate="no">
       <Head>
         <meta name="google" content="notranslate" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#00e5a0" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-title" content="DigitalCase" />
+        <link rel="apple-touch-icon" href="/icon-192.png" />
       </Head>
       <body>
         <Main />
