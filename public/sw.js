821287fcfb9141687ecfe5eb3af289efeb5026ff
Fidelio1234 <demivanlecce@gmail.com>
2026-06-17 22:11:10 +0200

    eliminato installa button

--- public/sw.js
+++ public/sw.js
@@ -1,11 +1,7 @@
-self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', () => self.clients.claim());
-
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    fetch(e.request).catch(() => {
-      // Se la rete è assente, restituisci una risposta offline
-      return new Response('Sei offline', { status: 503 });
-    })
-  );
-});
+self.addEventListener('install', (event) => {
+    self.skipWaiting();
+  });
+  
+  self.addEventListener('activate', (event) => {
+    event.waitUntil(self.clients.claim());
+  });
